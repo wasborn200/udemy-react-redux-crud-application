404ddd6d3e23b440288539d31d9a4e51691960d0
wasborn200 <archibald.ff1@gmail.com>
2021-06-27 13:52:00 +0900

    implement event new component

--- src/index.js
+++ src/index.js
@@ -3,30 +3,26 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import './index.css';
 import reducer from './reducers'
-
 import EventsIndex from './components/events_index';
-
+import EventsNew from './components/events_new';
 import reportWebVitals from './reportWebVitals';
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <EventsIndex />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/events/new" component={EventsNew} />
+        <Route exact path="/" component={EventsIndex} />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
 
 reportWebVitals();
-
-
-
-// udemyの講義ではこの内容
-// registerServiceWorkerが見つからないっぽい
-// import registerServiceWorker from './registerServiceWorker';
-//
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
